d5c409d6d4a73021f215dc038c5a7fa24d0f054c
WontonLov3r <danielhariantowu@gmail.com>
2023-10-04 14:50:30 -0400

    fixed numButPress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let preVal = ''
+let prevVal = ''
 let newVal = ''
 let resultVal = ''
 let mathOperator = ''
@@ -10,8 +10,32 @@ let decimalClicked = false
 let valMemStored = ''
 
 function numButPress(num) {
+    if (resultVal) {
+        //Start a new number
+        newVal = num
+        //reset to create a new result 
+        resultVal = ''
+    } else {
+        //used to block multiple decimals
+        if (num === '.') {
+            if (decimalClicked != true) {
+                //Take current value of newVal and add the character pressed
+                newVal += num
+                //sets decimal check var to true, wont allow here
+                decimalClicked = true
+            } 
+         } else {
+                newVal += num
+            }
+               
 
-}
+        }
+//update the display
+document.getElementById('entry').value = newVal
+console.log(newVal);    
+    }
+    
+
 
 function equalButPress() {
 
@@ -19,24 +43,24 @@ function equalButPress() {
 
 //clears everything except memory 
 function clearButPress() {
-    preVal = ''
+    prevVal = ''
     newVal = ''
     resultVal = ''
     mathOperator = ''
     decimalClicked = false
-    document.getElementById('entry').value = '0' 
+    document.getElementById('entry').value = '0'
 }
 
 //store the current value in #entry in valMemStored
 function copyButPress() {
-    valMemStored = document.getElementById('entry').value 
+    valMemStored = document.getElementById('entry').value
 }
 
 //If a value has been stored paste it in the #entry window and assign its as the newVal
 function pasteButPress() {
-    if(valMemStored) {
+    if (valMemStored) {
         document.getElementById('entry').value = valMemStored
-        newVal = ValMemStored 
+        newVal = valMemStored
     }
 }
 
